888dd3180bf5c12704fe1c2ece1808c97b65fbb2
MATUS LEDENYI <matus.ledenyi@gmail.com>
2022-07-13 22:21:34 +0200

    yuras plugins removed

--- nvim/lua/plug.lua
+++ nvim/lua/plug.lua
@@ -1,3 +1,5 @@
+--[[ lua/plug.lua ]]
+
 local fn = vim.fn
 
 -- Automatically install packer
@@ -64,82 +66,6 @@ return packer.startup(function(use)
       use 'airblade/vim-gitgutter'
 
       use {'jwoudenberg/elm-pair', rtp = 'editor-integrations/neovim'}
-      
-  -- === Yura's plugins ===
-      ---- fonts
-
-      ---- Send stuff to REPL 
-      --use "jpalardy/vim-slime"
-
-      -- use "kyazdani42/nvim-web-devicons" -- optional, for file icons
-
-      ---- completion plugins
-      --use "hrsh7th/nvim-cmp" -- The completion plugin
-      --use "hrsh7th/cmp-buffer" -- buffer completions
-      --use "hrsh7th/cmp-path" -- path completions
-      --use "hrsh7th/cmp-cmdline" -- cmdline completions
-      --use "saadparwaiz1/cmp_luasnip" -- snippet completions
-      --use "hrsh7th/cmp-nvim-lsp" -- LSP completion
-
-      ---- snippets
-      --use "L3MON4D3/LuaSnip" --snippet engine
-      --use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
-
-      ---- Language Server Protocol
-      ---- See https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-      --use "neovim/nvim-lspconfig" -- enable LSP
-      ---- TODO: Maybe remove?
-      ---- -- this is for scala (compatible with `nvim-lspconfig`)
-      ---- use {
-      ----   -- see https://github.com/scalameta/nvim-metals
-      ----   "scalameta/nvim-metals",
-      ----   requires = { "nvim-lua/plenary.nvim" },
-      ---- }
-
-      ---- File explorer
-      --use {
-      --  "kyazdani42/nvim-tree.lua",
-      --  requires = { "kyazdani42/nvim-web-devicons" },
-      --}
-
-      ---- Comments
-      --use "tpope/vim-commentary" -- Easy comments (gcc). This is used in combination with the `nvim-ts-context-commentstring` plugin
-
-      ---- Tree-sitter
-      ---- TODO: Don't forget to run `:TSInstall all` after installation of the plugin
-      --use "nvim-treesitter/nvim-treesitter" -- This is configured in `./treesitter.lua`
-
-      ---- Fuzzy Finder
-      --use "nvim-telescope/telescope.nvim"
-
-      ---- statsline/tabline
-      ---- use "itchyny/lightline.vim"
-
-      ---- Start screen
-      --use {
-      --  "goolord/alpha-nvim",
-      --  requires = { "kyazdani42/nvim-web-devicons" },
-      --  config = function ()
-      --      require("alpha").setup(require("alpha.themes.startify").config)
-      --  end
-      --}
-
-      ---- HTML/CSS
-      --use "mattn/emmet-vim" -- HTML and CSS speed coding.
-
-      ---- LaTeX
-      --use "LaTeX-Box-Team/LaTeX-Box" -- Lightweight toolbox for LaTeX.
-
-      ---- Markdown rendering server
-      ----  Run :LivedownPreview (to kill it :LivedownKill)
-      ----  or :LivedownToggle
-      ----  TODO: Don't forget ot `npm install -g livedown`
-      --use "shime/vim-livedown"
-
-      ---- Viewing binaries
-      --use "fidian/hexmode" -- hex/binary viewer. Just write :Hexmode
-  -- === Yura's plugins ===
-
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins.
